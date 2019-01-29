732ef00d09dcc40c653572f45aebfd34621d3abf
Marina <marisha_87@tut.by>
2019-01-29 20:43:57 +0300

    Add HomeTask1

---
+++ Task1/BasicTypes.java
@@ -0,0 +1,22 @@
+package core;
+
+public class BasicTypes {
+
+	public static void main(String[] args) {
+		byte a;
+		int b;
+		int c = 10;
+	//	int c1 = 0777, c2 = 0x32, c3 = 0b10;
+	//	short ds = 12;
+	//	long dL = 12;
+		double d = 999.25;
+	//	float dF = 99.25f;
+		char e = 'x';
+		boolean f = true;
+
+		a = 1;
+		b = 2;
+		System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f);
+	}
+
+}
