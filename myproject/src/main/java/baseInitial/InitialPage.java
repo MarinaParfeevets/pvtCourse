f0f69db49c34367469bcff14c139c99ec26000e2
Marina <marisha_87@tut.by>
2019-03-30 22:33:52 +0300

    Final Project

---
+++ myproject/src/main/java/baseInitial/InitialPage.java
@@ -0,0 +1,20 @@
+package baseInitial;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class InitialPage {
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+	private static final int LINK_PRESENSE_TIMEOUT = 20;
+
+	public InitialPage(WebDriver driver) {
+		this.driver = driver;
+		wait = new WebDriverWait(driver, LINK_PRESENSE_TIMEOUT);
+	}
+
+	public WebDriver setInitialProperty() {
+		driver.manage().window().maximize();
+		return driver;
+	}
+}
